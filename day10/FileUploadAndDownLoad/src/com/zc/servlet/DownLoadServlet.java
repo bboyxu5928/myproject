efa3ddc7e3465b8d7d67fb9bff4fd9d1686cd01d
yangxiaoting <L_tt0628@163.com>
2018-02-04 19:07:42 +0800

    mybaby1

--- day10/FileUploadAndDownLoad/src/com/zc/servlet/DownLoadServlet.java
+++ day10/FileUploadAndDownLoad/src/com/zc/servlet/DownLoadServlet.java
@@ -16,40 +16,41 @@ import javax.servlet.http.HttpServletResponse;
 public class DownLoadServlet extends HttpServlet{
 @Override
 protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	//得到要下载的文件名
+	//寰楀埌瑕佷笅杞界殑鏂囦欢鍚�
 	String fileName = req.getParameter("filename");
-	//上传的文件都保存在了/WEB-INF/upload目录的子目录下
+	//涓婁紶鐨勬枃浠堕兘淇濆瓨鍦ㄤ簡/WEB-INF/upload鐩綍鐨勫瓙鐩綍涓�
+	//mybaby1
 	String fileSaveRootPath = this.getServletContext().getRealPath("/WEB-INF/upload");
-	//通过文件名找出文件的所在目录
+	//閫氳繃鏂囦欢鍚嶆壘鍑烘枃浠剁殑鎵�鍦ㄧ洰褰�
 	String path = FileUtil.makePath(fileName, fileSaveRootPath);
 	System.out.println(path+File.separator+fileName);
-	//得到下载的文件
+	//寰楀埌涓嬭浇鐨勬枃浠�
 	File file = new File(path+File.separator+fileName);
-	//如果文件不存在
+	//濡傛灉鏂囦欢涓嶅瓨鍦�
 	if (!file.exists()) {
-		req.setAttribute("message", "您要下载的资源已经被删除！！");
+		req.setAttribute("message", "鎮ㄨ涓嬭浇鐨勮祫婧愬凡缁忚鍒犻櫎锛侊紒");
 		req.getRequestDispatcher("/message.jsp").forward(req, resp);
 		return;
 	}
-	//如果文件存在
+	//濡傛灉鏂囦欢瀛樺湪
 	String realname = fileName.substring(fileName.indexOf("_")+1);
-	//设置响应头
+	//璁剧疆鍝嶅簲澶�
 	resp.setHeader("content-disposition", "attachment;filename="+URLEncoder.encode(realname,"UTF-8"));
-	//读取要下载的文件，保存到文件输入流
+	//璇诲彇瑕佷笅杞界殑鏂囦欢锛屼繚瀛樺埌鏂囦欢杈撳叆娴�
 	FileInputStream in = new FileInputStream(path+File.separator+fileName);
-	//创建输出流
+	//鍒涘缓杈撳嚭娴�
 	OutputStream out= resp.getOutputStream();
-	//创建缓冲区
+	//鍒涘缓缂撳啿鍖�
 	byte buffer[] = new byte[1024];
 	int len = 0;
-	//循环将输入流中的内容读取到缓冲区内
+	//寰幆灏嗚緭鍏ユ祦涓殑鍐呭璇诲彇鍒扮紦鍐插尯鍐�
 	while ((len= in.read(buffer))>0) {
-		//输出缓冲区内容到浏览器中，实现文件下载
+		//杈撳嚭缂撳啿鍖哄唴瀹瑰埌娴忚鍣ㄤ腑锛屽疄鐜版枃浠朵笅杞�
 		out.write(buffer,0,len);
 	}
-	//关闭文件输入流
+	//鍏抽棴鏂囦欢杈撳叆娴�
 	in.close();
-	//关闭输出流
+	//鍏抽棴杈撳嚭娴�
 	out.close();
 	
 	
